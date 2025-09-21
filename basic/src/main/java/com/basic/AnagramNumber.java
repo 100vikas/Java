865f594f594194a1d628c9b4936cbbaeed4c46d4
100vikas <ughadevikas88@gmail.com>
2025-09-21 16:45:01 +0530

    Anagram string

---
+++ basic/src/main/java/com/basic/AnagramNumber.java
@@ -0,0 +1,36 @@
+package com.basic;
+
+public class AnagramNumber {
+
+	public static void main(String[] args) {
+
+		String str1 = "suudy";
+		String str2 = "dusty";
+
+		boolean status = true;
+		if (str1.length() == str2.length()) {
+			for (int i = 0; i < str1.length(); i++) {
+				status = false;
+				for (int j = 0; j < str2.length(); j++) {
+					if (str1.charAt(i) == str2.charAt(j)) {
+						str2 = str2.replace(str2.charAt(j), ' ');
+						status = true;
+						break;
+					}
+					if (!status) {
+						break;
+					}
+				}
+
+			}
+		} else {
+			status = false;
+		}
+
+		if (status) {
+			System.out.println("anagram");
+		} else {
+			System.out.println("not anagaram");
+		}
+	}
+}
